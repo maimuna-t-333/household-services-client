0a7abb4a50baae66feaebbf016c15518b4ffdf5b
maimuna-t-333 <maimuna.tabr@gmail.com>
2025-08-15 16:38:23 +0600

    contact

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import Hero from '../Components/Hero';
 import Services from './Servieces';
 import About from '../Components/About';
+import Contact from '../Components/Contact';
 
 const Home = () => {
     return (
         <div>
             <Hero></Hero>
-           
+            <About></About>
             <Services></Services>
 
-             <About></About>
+            
+             <Contact></Contact>
         </div>
     );
 };
